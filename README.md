d3ad7ea92f6aff4d785852bfa7239a144d43327e
nyuszika7h <nyuszika7h@gmail.com>
2021-01-28 15:21:51 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# decodeotp
+
+Extract TOTP/HOTP secret keys from Google Authenticator exports
+
+## Installation
+1. Install the dependencies: `pip install -r requirements.txt`
+2. Install the Protocol Buffers Compiler (protoc)
+3. Generate a Python file out of the `.proto` schema: `protoc --python_out=. google_auth.proto`
+
+## Usage
+1. Export your accounts from Google Authenticator
+2. Screenshot the QR code and decode it 
+3. Pass the URL to the script: `./decodeotp.py "otpauth-migration://offline?data=..."`
+
+The output will be in form of `otpauth://` URLs, one per line, like this:
+```
+otpauth://totp/ACME%20Inc.:John%20Smith?secret=ABCDEFGHIJKLMNOP&issuer=ACME%20Inc.&algorithm=SHA1&digits=6
+```
+
+## License
+[MIT License](LICENSE)
+
+`google_auth.proto` is provided by the [Aegis](https://github.com/alexbakker/Aegis) project. All other files contain original code.
---
+++ decodeotp.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python3
+
+import argparse
+import base64
+from urllib.parse import parse_qs, quote, urlencode, urlparse
+
+from google_auth_pb2 import MigrationPayload
+
+parser = argparse.ArgumentParser()
+parser.add_argument('url', help='otpauth migration URL')
+args = parser.parse_args()
+
+migration_payload = MigrationPayload()
+query = urlparse(args.url).query
+data = parse_qs(query)['data'][0]
+migration_payload.ParseFromString(base64.b64decode(data))
+
+for key in migration_payload.otp_parameters:
+    url = 'otpauth://'
+
+    if hasattr(key, 'type'):
+        if key.type == MigrationPayload.OTP_TYPE_TOTP:
+            url += 'totp'
+        elif key.type == MigrationPayload.OTP_TYPE_HOTP:
+            url += 'hotp'
+        else:
+            raise ValueError(f'Invalid type: {key.type!r}')
+    else:
+        raise ValueError('Missing type')
+
+    url += '/'
+
+    if getattr(key, 'issuer', None) and getattr(key, 'name', None):
+        url += ':'.join([quote(key.issuer), quote(key.name)])
+    elif getattr(key, 'issuer', None):
+        url += quote(key.issuer)
+    elif getattr(key, 'name', None):
+        url += quote(key.name)
+
+    params = {}
+
+    if hasattr(key, 'secret'):
+        params['secret'] = base64.b32encode(key.secret).rstrip(b'=')
+    else:
+        raise ValueError('Missing secret')
+
+    if getattr(key, 'issuer', None):
+        params['issuer'] = key.issuer
+
+    if hasattr(key, 'algorithm'):
+        if key.algorithm == MigrationPayload.ALGORITHM_MD5:
+            params['algorithm'] = 'MD5'
+        elif key.algorithm == MigrationPayload.ALGORITHM_SHA1:
+            params['algorithm'] = 'SHA1'
+        elif key.algorithm == MigrationPayload.ALGORITHM_SHA256:
+            params['algorithm'] = 'SHA256'
+        elif key.algorithm == MigrationPayload.ALGORITHM_SHA512:
+            params['algorithm'] = 'SHA512'
+        elif key.algorithm != MigrationPayload.ALGORITHM_UNSPECIFIED:
+            raise ValueError(f'Invalid algorithm: {key.algorithm!r}')
+
+    if hasattr(key, 'digits'):
+        if key.digits == MigrationPayload.DIGIT_COUNT_SIX:
+            params['digits'] = 6
+        elif key.digits == MigrationPayload.DIGIT_COUNT_EIGHT:
+            params['digits'] = 8
+        elif key.digits != MigrationPayload.DIGIT_COUNT_UNSPECIFIED:
+            raise ValueError(f'Invalid digit count: {key.digits!r}')
+
+    if key.type == MigrationPayload.OTP_TYPE_HOTP:
+        if hasattr(key, 'counter'):
+            params['counter'] = key.counter
+        else:
+            raise ValueError('Missing counter')
+
+    if hasattr(key, 'period') and key.type == MigrationPayload.OTP_TYPE_TOTP:
+        params['period'] = key.period
+
+    url += '?' + urlencode(params)
+    print(url)
